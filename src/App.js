95bff1bb48070eb04443815efd0fb64dd28a9c2a
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-03-08 15:47:47 +0100

    Component update in action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from "./components/UI/Button/Button"
 
 import './App.css';
 
 function App() {
+
+  const [showTex, setShowText] =  useState(false)
+
+  const showtexthandler = () => {
+    setShowText(previous => !previous)
+  }
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showTex && <p>This is new</p>}
+      <Button onClick={showtexthandler}>Toggle</Button>
     </div>
   );
 }
